aefc6c1465a056b570b5074b8fe917bfbe596b92
Bruno Boi <bruno.boi@bobtopus.eu>
2026-05-07 07:38:40 +0200

    config: add @shared alias for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@shared': resolve(__dirname, 'shared'),
+    },
+  },
   test: {
     globals: true,
     environment: 'node',
